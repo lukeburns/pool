93fcffc24ded53c3ca021ac433b258942b3a1d58
Luke Burns <luke@lukeburns.com>
2014-07-18 22:53:43 -0400

    only accept returned streams

--- index.js
+++ index.js
@@ -10,15 +10,7 @@ function pool (cb) { // proposal: options? { toString: Bool, pass: Bool, ... }
 		callback();
 	}, function (callback) {
 		var res = cb(chunks.join('').toString());
-		if (typeof res !== 'undefined' && !(res instanceof require('stream').Readable || res.readable)) {
-			if (typeof res === 'object') {
-				this.push(JSON.stringify(res));
-			} else if (typeof String(res) === 'string') {
-				this.push(String(res));
-			} else {
-				this.push(null);
-			}
-		} else if (typeof res !== 'undefined' && (res instanceof require('stream').Readable || res.readable)) {
+		if (res && (res instanceof require('stream').Readable || res.readable)) {
 			res.on("data", this.push.bind(this));
       res.on("end", this.emit.bind(this, "end"));
       res.on("error", this.emit.bind(this, "error"));
